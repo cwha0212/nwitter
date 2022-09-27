6cc5768e30b19d0ab6fba41adb1bc80fa7dc0fd7
cwha0212 <cwha02120212@gmail.com>
2022-09-27 22:32:13 +0900

    22

--- src/components/App.js
+++ src/components/App.js
@@ -4,24 +4,24 @@ import { authService } from "fbase";
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
   useEffect(() => {
-    authService.onAuthStateChanged((user) =>{
-      if(user){
-        setIsLoggedIn(true);
-      }
-      else{
-        setIsLoggedIn(false);
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setUserObj(user);
       }
       setInit(true);
     });
   }, []);
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "init..."}
+      {init ? (
+        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
+      ) : (
+        "Initializing..."
+      )}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
 }
-
 export default App;
